50c61e7dbbc421dcbe40df03c63e76a0f34fadea
aurorabora <borabora.choi@gmail.com>
2018-05-10 15:27:27 -0700

    npm scripts setup

---
+++ client/src/components/test.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Test extends Component {
+
+    componentDidMount(){
+        this.getUserData();
+    }
+
+    async getUserData(){
+        const response = await axios.get('http://localhost:9000/user-data');
+        console.log('User Data Respnse:', response);
+    }
+
+    render(){
+        return <h1> Test component</h1>
+    }
+}
+
+export default Test;
