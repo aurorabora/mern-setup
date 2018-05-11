8d6898966fb8ba272ffba90e9ad0160bd42d1e16
aurorabora <borabora.choi@gmail.com>
2018-05-10 17:45:10 -0700

    finished mern

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { resolve }= require('path')
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data', (req, res) =>{
+
+app.post('/api/send-data', (req,res)=>{
+     console.log('Data sent:', req.body);
+
+     res.send({success: true, mirror: req.body});
+});
+
+app.get('/api/user-data', (req, res) => {
     const user = {
         name: 'Jim Bob',
-        email:'jim@jimbob.com',
-
+        email: 'jim@jimbob.com',
     }
 
     res.send(user);
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1> something else!  </h1>');
+app.get('/api/get-article', (req, res) => {
+    const article = {
+        title: 'Some Awesome Article',
+        author: 'John Walker',
+        content: 'lorem ipsum'
+    }
+
+    res.send(article);
+});
+
+app.get('*', (req, res)=> {
+    res.sendFile(resolve(_dirname, 'client', 'dist', 'index.html'));
 
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log('Server running at localhost:' + PORT);
-}).on('error', (err) =>{
+}).on('error', (err) => {
     console.log('Server Error:', err.message);
     console.log('Do you already have a server running on PORT:' + PORT);
 });
